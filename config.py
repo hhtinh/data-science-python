58ce751074bb65fae99cb95b2e4226a9f761b5ba
Huỳnh Hữu Tính (VCM-VM) <tinhhh@vingroup.net>
2019-10-24 10:25:52 +0700

    add upload_gs xml_to_json

---
+++ config.py
@@ -0,0 +1,10 @@
+"""Google Cloud Storage Configuration."""
+from os import environ
+
+# Google Cloud Storage
+bucketURL = environ.get('GCP_BUCKET_URL')
+bucketName = environ.get('GCP_BUCKET_NAME')
+bucketFolder = environ.get('GCP_BUCKET_FOLDER_NAME')
+
+# Data
+localFolder = environ.get('LOCAL_FOLDER')
