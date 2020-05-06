2e59dccb403dc40908537965b441f2ab6b669d94
hhtinh <hhtinh@gmail.com>
2020-05-06 17:24:11 +0700

    add tensorflow gpu test

---
+++ tensorflow-gpu-test.py
@@ -0,0 +1,25 @@
+
+from tensorflow.python.client import device_lib
+
+local_device_protos = device_lib.list_local_devices()
+
+for x in local_device_protos: print(x.name)
+
+
+import tensorflow as tf
+# Creates a graph.
+with tf.device('/device:GPU:0'):
+  a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+  b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
+
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+get_available_gpus()
